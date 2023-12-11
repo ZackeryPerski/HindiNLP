49a5eaf12f21864c5086372e6733769d7a37c6ea
Zackery Perski <zackeryperski@gmail.com>
2023-12-10 22:00:33 -0500

    Further updates to .csv and to the main project.

--- Scripts/hindiparser.py
+++ Scripts/hindiparser.py
@@ -1,13 +1,5 @@
 import pandas as pd
 import unittest
-import re
-
-'''
-Things that can be null:
-subject
-object
-But not both...
-'''
 
 subjects = []
 objects = []
@@ -21,31 +13,23 @@ verb_supports = []
 
 def introduction():
     print("Welcome to the Hindi Parser!")
-    print("")#TODO
     print("This project was created by Akarsh, Hiranmai, Uma, and Zack for Foundations of Automata at Eastern Michigan University")
-    print("")#TODO
     print("This is a simple parser, designed to check the validity of statements made for a subset of Hindi, particularly focused on present tense sentence structures.")
-    print("")#TODO
     print("When prompted for a sentence you will need to write in Hindi. The program will evaluate your sentence and tell you if your sentence was valid.")
-    print("")#TODO
     print("When you want to quit, type the word 'quit' on the prompt screen.")
-    print("")#TODO
 
 def mainFunction():
     while True:
         print("Enter a phrase in present tense Hindi to be checked")
-        print("")#TODO
-        phrase = input("Phrase/#TODO: ")
+        phrase = input("Phrase: ")
         phrase = phrase.strip() #removes leading and trailing whitespace.
         if(phrase == 'quit'):
             quit()
         else:
-            if(isHindi(phrase)):
+            if(inputPhrases(phrase)):
                 print("That was a valid phrase!")
-                print("")#TODO
             else:
                 print("That was an invalid phrase!")
-                print("")
 
 def parseAdjectives(words):
     global adjectives
@@ -76,7 +60,6 @@ def parseObjectAndDescription(words):
             unparsedWords.pop()
         except ValueError:
             return (unparsedWords, False, fault_position) #More words left to parse, but the first word is not an object word, meaning that this is a lambda transition out of OBJECT_AND_DESCRIPTION in the grammar.
-    
     #Here we had at least one object word, we're going to remove additional object words here, if there are any before attempting to remove adjectives.
     while len(unparsedWords)>0:
         current_word = unparsedWords[-1]
@@ -127,7 +110,6 @@ def parseSubjectString(words):
             unparsedWords.pop()
         except ValueError:
             return (unparsedWords, True, len(unparsedWords)-1) #fault out here. At this point we need to parse at least one subject word to proceed.
-        
     #Here we had at least one subject word, we're going to remove additional object words here, if there are any before attempting to remove adjectives.
     while len(unparsedWords)>0:
         current_word = unparsedWords[-1]
@@ -137,29 +119,40 @@ def parseSubjectString(words):
         except ValueError:
             break
     return (unparsedWords, False, -1)
-    
+
+
+def parseComplexVerbString(words):
+    global verb_supports, manner, verbs
+    #Step1 Remove verb stem, if it exists.
+    try:
+        verb_supports.index(words[len(words)-1]) #If non-existent, it's fine. but if it does exist, then we pop it off and move on.
+        words.pop() #Removes the verb_supports word from the end of the derivation. I've been told there is only ever 1 of these after a verb, hense the non-recursion here. 
+    except ValueError:
+        print("No verb_supports, moving on.")
+    #Step2 Remove manner word, if it exists.
+    try:
+        manner.index(words[len(words)-1])
+        words.pop() #Consumes the manner word from the string.
+    except ValueError:
+        print("No manner words, moving on.")
+    #Step3 Remove at least one verb.
+    try:
+        verbs.index(words[len(words)-1]) #Checks to see if the word exists within the list of verbs. If this doesn't throw a ValueError Exception, then it exists, and we continue.
+        words.pop()
+    except ValueError:
+        return (words, True, len(words)-1)
+    #Step4 Remove any additional verbs utilizing a while loop.
+    try:
+        while len(words)>0:
+            current_word = words[-1]
+            verbs.index(current_word)
+            words.pop()
+    except ValueError:
+        return (words, False, -1)
 
 
 def validSentenceStructure(words):
     global subjects, objects, verbs, adverbs, frequency_adverbs, adjectives, time, manner
-    '''
-        Parsing strategy: There are four required productions needed to have a complete formal present tense sentence:
-        1. Subject.
-        2. Verb.
-        3. A word indicative of present tense.
-        4. Proper punctuation.
-
-        3 out of 4 of these requirements are needed to be performed at the end of the sentence, so our strategy is to derive those first before going into the weeds with recursive productions.
-        As we read these productions, we consume the associated characters/words, so that progress is being made.
-
-        Once we have passed these 3 requirements, we dive into potentially recursive statements. The way I intend to handle this will be according to the grammar on the sheet.
-        For the recursive portions of the grammar they are linearly recursive, with expansion on the right. That means that it's going to be easier to start consuming from the right, and stopping
-        when we've run out of words that fit the particular mold going towards the left. This will also help us spot when certain types of issues occur, such as end of input words.
-
-        This function returns two things partnered together as a tuple: (<bool>pass_status,<string>message)
-        This will tell the user if the phrase was correct, or if it was incorrect, what went wrong. 
-    '''
-
     #We start the parser with basic spot checking. There are certain minimal rules to be followed before we seek out recursive parts of the grammar.
     #First check, a formal present tense sentence needs at least 3 words in the form: subject verb tense+punctuation.
     if(len(words)<=2):
@@ -179,23 +172,12 @@ def validSentenceStructure(words):
 
     #Fourth check, the word before the tense word is our second required word component. Unlike prior components, verbs may have a supporting word that plays a similar role to our
     #verbs ending in 'ing'
-    try:
-        verb_supports.index(words[len(words)-1]) #If non-existent, it's fine. but if it does exist, then we pop it off and move on.
-        words.pop() #Removes the verb_supports word from the end of the derivation. I've been told there is only ever 1 of these after a verb, hense the non-recursion here. 
-    except ValueError:
-        print("No verb_supports, moving on.")
-    try:
-        manner.index(words[len(words)-1])
-        words.pop() #Consumes the manner word from the string.
-    except ValueError:
-        print("No manner words, moving on.")
-    try:
-        verbs.index(words[len(words)-1]) #Checks to see if the word exists within the list of verbs. If this doesn't throw a ValueError Exception, then it exists, and we continue.
-        words.pop()
-    except ValueError:
-        return (False, "A complete sentence requires at least one verb (with optional verb manner and assist word), placed before the tense word in the sentence.")
-    #At this point, we have passed all of the easy checks, and have potentially 1 or more words left to check.
-
+    words, fault, faultPos = parseComplexVerbString(words)
+    if(len(words)==0):
+        return (False, "No subject present in the sentence.")
+    if(fault):
+        return (False, "No verb present in string, expected verb at position: "+str(faultPos)+". Saw the word: "+words[faultPos]+" instead.")
+    
     #At this step we may complete, as there is a lot of overlap between an object and it's description and a subject and it's description.
     #If it's actually an adj_phrase and subject and not an adj_phrase and an object, we would be out of words by the time it's done parsing.
     words, failed, position = parseObjectAndDescription(words) #returns a list of unparsed words.
@@ -269,81 +251,108 @@ def loadWords():
     return
 
 
-def isHindi(sentence):
+def isHindiSentence(sentence):
     words = sentence.split(" ")
     status, message = validSentenceStructure(words)
     evaluation = "Parsing Succssful" if status else "Parsing Failed: " + message
     print(evaluation)
     return status
 
+def inputPhrases(input):
+    try:
+        input = input.strip()
+    except:
+        return (False, "Input was not a valid string.")
+    #fails if input is garbage.
+    sentences = []
+    current_sentence = ""
+    #construct individual sentences from the larger input.
+    for i in range(len(input)):
+        char = input[i]
+        current_sentence = current_sentence + char
+        if char == "|" or char == "?" or char == "!":
+            sentences.append(current_sentence)
+            current_sentence = ""
+    #test individual sentences
+    for sentence in sentences:
+        sentence = sentence.strip()
+        status = isHindiSentence(sentence)
+        if status == False:
+            return False
+    return True
+
+
 
 class TestStringMethods(unittest.TestCase):
 
     def test_real_sentence1(self): #passed. 
-        self.assertTrue(isHindi("वह घर पर है|"))
+        self.assertTrue(inputPhrases("वह घर पर है|"))
 
-    #def test_real_sentence2(self):
-    #    self.assertTrue(isHindi())
+    def test_real_sentence2(self): #passed
+        self.assertTrue(inputPhrases("स्कूल बच्चों खेल रहे हैं|"))
 
-    #def test_real_sentence3(self):
-    #    self.assertTrue(isHindi())
+    def test_real_sentence3(self): #passed.
+        self.assertTrue(inputPhrases("वह टीवी स्क्रीन देख रहा है|"))
     
     def test_real_sentence4(self): #passed.
-        self.assertTrue(isHindi("सूरज उगता है|"))
+        self.assertTrue(inputPhrases("सूरज उगता है|"))
 
     def test_real_sentence5(self): #passed.
-        self.assertTrue(isHindi("बच्चे खेल रहे हैं|"))
+        self.assertTrue(inputPhrases("बच्चे खेल रहे हैं|"))
 
-    def test_real_sentence6(self):
-        self.assertTrue(isHindi("महिलाएँ बाज़ार में खरीदारी करती हैं|"))
+    def test_real_sentence6(self): #passed!
+        self.assertTrue(inputPhrases("महिलाएँ बाज़ार में खरीदारी करती हैं|"))
 
     def test_real_sentence7(self): #passed.
-        self.assertTrue(isHindi("कुत्ता भोंक रहा है|"))
-
-    def test_real_sentence8(self):
-        self.assertTrue(isHindi("बच्चों ने गाना गाया|")) #Incorrectly labeled, this is a past tense sentence.
+        self.assertTrue(inputPhrases("कुत्ता भोंक रहा है|"))
 
     def test_real_sentence9(self): #passed.
-        self.assertTrue(isHindi("मेरे दोस्त पार्क में खेल रहे हैं|"))
+        self.assertTrue(inputPhrases("मेरे दोस्त पार्क में खेल रहे हैं|"))
 
     def test_real_sentence10(self): #passed.
-        self.assertTrue(isHindi("वह खुशी से हंस रहा है|"))
+        self.assertTrue(inputPhrases("वह खुशी से हंस रहा है|"))
+
+    def test_real_paragraph1(self): #passed.
+        self.assertTrue(inputPhrases("वह खुशी से हंस रहा है| मेरे दोस्त पार्क में खेल रहे हैं|"))
+
+    def test_real_paragraph2(self):
+        self.assertTrue(inputPhrases("वह सुंदर दिखती है| वह स्वादिष्ट खाना बनाती है| वह अच्छी लड़की है|"))
 
     def test_bad_sentence1(self): #passed
-        self.assertFalse(isHindi("मैंने सूरज खरीदती|"))
+        self.assertFalse(inputPhrases("मैंने सूरज खरीदती|"))
 
     def test_bad_sentence2(self): #passed
-        self.assertFalse(isHindi("घर में बाज गा रही है|"))
+        self.assertFalse(inputPhrases("घर में बाज गा रही है|"))
 
     def test_bad_sentence3(self): #passed
-        self.assertFalse(isHindi("पेड़ पर खाना खाया|"))
+        self.assertFalse(inputPhrases("पेड़ पर खाना खाया|"))
 
     def test_bad_sentence4(self): #passed
-        self.assertFalse(isHindi("बिल्ली रोती है|"))
+        self.assertFalse(inputPhrases("बिल्ली रोती है|"))
 
     def test_bad_sentence5(self): #passed
-        self.assertFalse(isHindi("स्कूल बच्चों खेल रहे हैं|"))
+        self.assertFalse(inputPhrases("बाज़ार महिलाएँ खरीदते हैं|"))
 
     def test_bad_sentence6(self): #passed
-        self.assertFalse(isHindi("बाज़ार महिलाएँ खरीदते हैं|"))
+        self.assertFalse(inputPhrases("कुत्ता गाना गा रहा है|"))
 
     def test_bad_sentence7(self): #passed
-        self.assertFalse(isHindi("कुत्ता गाना गा रहा है|"))
+        self.assertFalse(inputPhrases("बच्चों किताब पढ़ते हैं|"))
 
     def test_bad_sentence8(self): #passed
-        self.assertFalse(isHindi("बच्चों किताब पढ़ते हैं|"))
+        self.assertFalse(inputPhrases("मेरे पार्क खेलते दोस्त हैं|"))
 
     def test_bad_sentence9(self): #passed
-        self.assertFalse(isHindi("मेरे पार्क खेलते दोस्त हैं|"))
+        self.assertFalse(inputPhrases("वह हंसी से खुश है|"))
 
     def test_bad_sentence10(self): #passed
-        self.assertFalse(isHindi("वह हंसी से खुश है|"))
+        self.assertFalse(inputPhrases("मैंने किताब पढ़ी|"))
 
     def test_bad_sentence11(self): #passed
-        self.assertFalse(isHindi("मैंने किताब पढ़ी|"))
+        self.assertFalse(inputPhrases("चिड़िया चली गई|"))
 
-    def test_bad_sentence12(self): #passed
-        self.assertFalse(isHindi("चिड़िया चली गई|"))
+    def test_bad_sentence12(self): #passed.
+        self.assertFalse(inputPhrases("बच्चों ने गाना गाया|"))
     
 
 
